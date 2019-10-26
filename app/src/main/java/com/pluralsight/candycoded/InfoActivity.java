1a2b1eb93a930724b7906153658ebc5746cb4cda
Rock Okechukwu <rockok10@gmail.com>
2019-10-26 14:36:52 +0100

    added createPhoneIntent method

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -38,6 +38,11 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
 
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
